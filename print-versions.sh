d8e4517a240d683c3cc9d3ee3efd6bc565ce4dcf
knutole <knutole@mapic.io>
2018-10-23 13:32:25 +0000

    add script to print versions

--- print-versions.sh
+++ print-versions.sh
@@ -3,12 +3,15 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+echo ""
 echo -e "${RED}Shiny Server version:${NC}"
 shiny-server --version
 
+echo ""
 echo -e "${RED}R version:${NC}"
 R --version
 
+echo ""
 echo -e "${RED}R packages versions:${NC}"
 R -q --no-save -e 'ip <- as.data.frame(installed.packages()[,c(1,3:4)]); rownames(ip) <- NULL; ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]; print(ip, row.names=FALSE)'
 
