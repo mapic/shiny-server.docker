8104f343ee7138a206b82dda86a91c3e62cdae14
knutole <knutole@mapic.io>
2018-10-23 13:16:44 +0000

    cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,5 @@
 
 export SHINY_LOG_LEVEL=DEBUG
 
-# configure license manager
-# /opt/shiny-server/bin/license-manager license-server $SHINY_LICENSE_SERVER:$SHINY_LICENSE_PORT
-
 # start shiny
 shiny-server --pidfile=/var/run/shiny-server.pid
