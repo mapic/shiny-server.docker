f0fe7c7a79910e6034eb266ed350259cd57c29ca
knutole <knutole@mapic.io>
2019-12-04 17:15:13 +0000

    chown log files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,8 @@ export SHINY_LOG_LEVEL=DEBUG
 # chown apps folder
 chown 1001:www-data /srv/shiny-server/apps
 
+# chown logs folder
+chown -R 1001:www-data /var/log/shiny-server/
+
 # start shiny
 shiny-server --pidfile=/var/run/shiny-server.pid
